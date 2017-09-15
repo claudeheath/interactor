b291c1223d340dd640bdc87625533dea32624cfa
Peter Cook <peter@animateddata.co.uk>
2017-09-15 16:25:30 +0100

    Improve bounding box calculation in image download + include node images (doesn't work on Firefox)

--- interactor_app/interactor-spa/src/Screenshot/index.js
+++ interactor_app/interactor-spa/src/Screenshot/index.js
@@ -4,16 +4,26 @@ import Helpers from '../Helpers'
 
 const Screenshot = {}
 
-function getSize(nodes) {
+function getBounds(nodes) {
+  var minX = d3.min(nodes, d => d.x - d.size)
+  var minY = d3.min(nodes, d => d.y - d.size)
   var maxX = d3.max(nodes, d => d.x + d.size)
   var maxY = d3.max(nodes, d => d.y + d.size)
-  return {x: maxX + 100, y: maxY + 100}
+  var padding = 50
+  return {minX: minX - padding, minY: minY - padding, maxX: maxX + padding, maxY: maxY + padding}
 }
 
-function drawBackground(context, size) {
+function drawBackground(context, bounds) {
+  context.save()
+
+  context.strokeStyle = 'white';
+  context.fillStyle = 'white';
+
   context.beginPath()
-  context.rect(0, 0, size.x, size.y)
+  context.rect(-10, -10, bounds.maxX - bounds.minX + 20, bounds.maxY - bounds.minY + 20)
   context.fill()
+
+  context.restore()
 }
 
 function arrowhead(context, d) {
@@ -60,9 +70,10 @@ function drawLinks(context, links, lu) {
   _.each(links, d => {
     context.save()
 
-    const p0 = {x: lu[d.source].x, y: lu[d.source].y};
-    const p1 = {x: lu[d.target].x, y: lu[d.target].y};
-    const mid = Helpers.quadraticMidpoint(p0, p1)
+    const p0 = {x: lu[d.source].x, y: lu[d.source].y}
+    const p1 = {x: lu[d.target].x, y: lu[d.target].y}
+    const k = d.curvature === undefined ? 0.2 : d.curvature
+    const mid = Helpers.quadraticMidpoint(p0, p1, k)
 
     let width = 1
     const gap = (100 - d.opacity) / 5
@@ -80,7 +91,7 @@ function drawLinks(context, links, lu) {
     // Arrowhead
     context.save()
     const rot = Helpers.getRotationFromSource(d, p0, p1)
-    const pos = Helpers.getQuadraticMidpoint(p0, p1)
+    const pos = Helpers.getQuadraticMidpoint(p0, p1, k)
 
     // const pos = Helpers.getMidpointOfPath(g.select('.ispa-line').node())
     context.translate(pos.x, pos.y)
@@ -92,56 +103,65 @@ function drawLinks(context, links, lu) {
 }
 
 function drawNodes(context, nodes) {
-  _.each(nodes, d => {
-    context.save()
-
-    let width = (100 - d.opacity) / 5
-    width = width > 5 ? 5 : width
-    const gap = (100 - d.opacity) / 5
-    context.setLineDash([width, gap])
-
-    context.lineWidth = d.width
-    context.fillStyle = 'white'
-
-    context.beginPath()
-    context.arc(d.x, d.y, d.size, 0, 2 * Math.PI)
-    context.stroke()
-    context.fill()
-
-    context.font = "14px 'Oswald', sans-serif"
-    context.fillStyle = '#333'
-    context.textAlign = "center"
-    context.fillText(d.name, d.x, d.y + d.size + 16)
-
-    context.restore()
-  })
-
-
-  // d3.select('.ispa-graphview .ispa-nodes')
-  //   .selectAll('.ispa-node')
-  //   .each(function(d) {
-  //     context.save()
+  // _.each(nodes, d => {
+  //   context.save()
   //
-  //     let width = (100 - d.opacity) / 5
-  //     width = width > 5 ? 5 : width
-  //     const gap = (100 - d.opacity) / 5
-  //     context.setLineDash([width, gap])
+  //   let width = (100 - d.opacity) / 5
+  //   width = width > 5 ? 5 : width
+  //   const gap = (100 - d.opacity) / 5
+  //   context.setLineDash([width, gap])
   //
-  //     context.beginPath()
-  //     context.arc(d.x, d.y, d.size, 0, 2 * Math.PI)
-  //     context.stroke()
+  //   context.lineWidth = d.width
+  //   context.fillStyle = 'white'
   //
-  //     // if(d.imageUrl) {
-  //     //   context.beginPath()
-  //     //   context.arc(d.x, d.y, d.size >= 4 ? d.size - 4 : 0, 0, 2 * Math.PI)
-  //     //   context.clip()
-  //     //
-  //     //   var img = d3.select(this).select('image').node()
-  //     //   context.drawImage(img, d.x - d.size, d.y - d.size, d.size * 2, d.size * 2)
-  //     // }
+  //   context.beginPath()
+  //   context.arc(d.x, d.y, d.size, 0, 2 * Math.PI)
+  //   context.stroke()
+  //   context.fill()
   //
-  //     context.restore()
-  //   })
+  //   context.font = "14px 'Oswald', sans-serif"
+  //   context.fillStyle = '#333'
+  //   context.textAlign = "center"
+  //   context.fillText(d.name, d.x, d.y + d.size + 16)
+  //
+  //   context.restore()
+  // })
+
+
+  d3.select('.ispa-graphview .ispa-nodes')
+    .selectAll('.ispa-node')
+    .each(function(d) {
+      context.save()
+
+      let width = (100 - d.opacity) / 5
+      width = width > 5 ? 5 : width
+      const gap = (100 - d.opacity) / 5
+      context.setLineDash([width, gap])
+
+      context.lineWidth = d.width
+      context.fillStyle = 'white'
+
+      context.beginPath()
+      context.arc(d.x, d.y, d.size, 0, 2 * Math.PI)
+      context.stroke()
+      context.fill()
+
+      context.font = "14px 'Oswald', sans-serif"
+      context.fillStyle = '#333'
+      context.textAlign = "center"
+      context.fillText(d.name, d.x, d.y + d.size + 16)
+
+      if(d.imageUrl) {
+        context.beginPath()
+        context.arc(d.x, d.y, d.size >= 4 ? d.size - 4 : 0, 0, 2 * Math.PI)
+        context.clip()
+
+        var img = d3.select(this).select('image').node()
+        context.drawImage(img, d.x - d.size, d.y - d.size, d.size * 2, d.size * 2)
+      }
+
+      context.restore()
+    })
 }
 
 Screenshot.save = function(link, state, filename) {
@@ -149,15 +169,20 @@ Screenshot.save = function(link, state, filename) {
   const canvas = document.createElement('canvas')
   const context = canvas.getContext('2d')
 
-  const size = getSize(project.representation.nodes)
-  canvas.width = size.x
-  canvas.height = size.y
+  const bounds = getBounds(project.representation.nodes)
+
+  const scaleFactor = 2
+
+  canvas.width = scaleFactor * (bounds.maxX - bounds.minX)
+  canvas.height = scaleFactor * (bounds.maxY - bounds.minY)
 
   context.lineWidth = 1;
   context.strokeStyle = '#333';
   context.fillStyle = 'white';
 
-  drawBackground(context, size)
+  context.scale(scaleFactor, scaleFactor)
+  drawBackground(context, bounds)
+  context.translate(-bounds.minX, -bounds.minY)
   drawGroups(context, project.representation.groups, state.nodeLU)
   drawLinks(context, project.representation.links, state.nodeLU)
   drawNodes(context, project.representation.nodes)
